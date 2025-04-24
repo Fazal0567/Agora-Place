39a353f50505d3c24b2c279832573cff293ac4dd
bhavy-sharma <bhavymay18@gmail.com>
2025-04-25 02:15:37 +0530

    Adding Basic File Structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+app.listen(3000, () => {
+  console.log('Agora Place running on http://localhost:3000');
+});
