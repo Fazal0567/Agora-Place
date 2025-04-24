f2f3de9b61ae22f189c7a65a9e1292796804f730
bhavy-sharma <bhavymay18@gmail.com>
2025-04-25 03:27:37 +0530

    Add Product Page

--- app.js
+++ app.js
@@ -3,13 +3,33 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',  (req, res) => {
-  res.render('pages/home');
+app.get("/", (req, res) => {
+  const products = [
+    {
+      id: "1",
+      name: "iPhone 15",
+      description: "Latest Apple iPhone",
+      price: 999,
+      image: "/images/iphone15.jpg"
+    },
+    {
+      id: "2",
+      name: "Samsung Galaxy S23",
+      description: "High-end Android phone",
+      price: 899,
+      image: "/images/galaxy-s23.jpg"
+    },
+  ];
+
+  res.render("pages/home", { products });
 });
+
 
 app.listen(3000, () => {
   console.log('Agora Place running on http://localhost:3000');
